bb434acf24fa1b569e7ede525aa586822387b15a
tony <172993974@qq.com>
2018-04-05 17:15:27 +0800

    'add'

--- service/controllers/index.js
+++ service/controllers/index.js
@@ -2,28 +2,33 @@
  * 控制器
  */
 
-const request = require("request")
+const request = require("request");
 
 const controllers = {
   // 获取IP地址
-  async getIp (ctx) {
+  async getIp(ctx) {
     let ip = ctx.ip;
+    ip = ip.substring(ip.lastIndexOf("f:") + 2);
     ctx.response.type = "application/json";
     let result = await request({
-      url: "http://apis.juhe.cn/ip/ipNew?ip=" +
+      url:
+        "http://apis.juhe.cn/ip/ipNew?ip=" +
         ip +
         "&key=ed2b36c92c3d48eff07f2fe153fe1ecd"
     });
     ctx.response.body = result;
   },
   // 获取聊天信息
-  async getAI (msg) {
+  async getAI(ctx) {
     ctx.response.type = "application/json";
+    msg = ctx.request.query.msg || "";
     let result = await request({
-      url: 'http://api.qingyunke.com/api.php?key=free&appid=0&msg=' + encodeURI(msg)
+      url:
+        "http://api.qingyunke.com/api.php?key=free&appid=0&msg=" +
+        encodeURI(msg)
     });
     ctx.response.body = result;
   }
-}
+};
 
-module.exports = controllers
+module.exports = controllers;
--- service/routers/api.js
+++ service/routers/api.js
@@ -2,8 +2,10 @@ const router = require("koa-router")();
 
 const controller = require("../controllers/index");
 
-module.exports = router.get("/get-ip", controller.getIp).get("/get-ai", async ctx => {
-  ctx.response.type = "application/json";
-  let result = await controller.getAI();
-  ctx.response.body = result;
-});
+module.exports = router
+  .get("/get-ip", controller.getIp)
+  .get("/get-ai", async ctx => {
+    ctx.response.type = "application/json";
+    let result = await controller.getAI(ctx);
+    ctx.response.body = result;
+  });
