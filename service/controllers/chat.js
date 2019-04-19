e857b8809afce8987654a806c933a75308971b60
tony <172993974@qq.com>
2019-04-19 18:08:45 +0800

    'chat类'

--- service/controllers/chat.js
+++ service/controllers/chat.js
@@ -1,3 +1,28 @@
-module.exports = {
+export default class Chat {
+    static async getMessage (userId, otherUserId) {
 
+    }
+
+    /**
+     * 返回时要更新状态
+     * @param {*} userId 
+     * @param {*} otherUserId 
+     */
+    static async updateEnterChat (userId, otherUserId) {
+
+    }
+
+    /**
+     * 屏蔽消息
+     * @param {*} userId 
+     * @param {*} otherUserId 
+     * @param {*} status 
+     */
+    static async updateShield (userId, otherUserId, status) {
+
+    }
+
+    static async updateSpecial (userId, otherUserId, status) {
+
+    }
 }
