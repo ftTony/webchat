c0abeaa179015126b9010c40168f2c606595d359
tony <172993974@qq.com>
2019-05-02 11:11:53 +0800

    '路由'

---
+++ client/components/Chatting.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    聊天室
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
---
+++ client/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/Login'
+import Chatting from '../components/Chatting'
+import AI from '../components/AI'
+
+Vue.use(Router)
+
+const router = new Router({
+
+})
+
+export default router
