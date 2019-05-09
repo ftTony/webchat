28707f1335f87ee425f31fc39c3e95d476d3f4bb
tony <172993974@qq.com>
2019-05-09 11:00:17 +0800

    '完善相关功能'

--- service/app.js
+++ service/app.js
@@ -3,6 +3,7 @@ import IO from 'koa-socket-2'
 import koaSend from 'koa-send'
 import koaStatic from 'koa-static'
 import bodyParser from 'koa-bodyparser'
+import koaSession from 'koa-session'
 import Busboy from 'busboy'
 
 
@@ -15,6 +16,21 @@ const io = new IO({
     }
 });
 
+const SESSION_CONFIG = {
+    key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+    /** (number || 'session') maxAge in ms (default is 1 days) */
+    /** 'session' will result in a cookie that expires when session/browser is closed */
+    /** Warning: If a session cookie is stolen, this cookie will never expire */
+    maxAge: 86400000,
+    autoCommit: true, /** (boolean) automatically commit headers (default true) */
+    overwrite: true, /** (boolean) can overwrite or not (default true) */
+    httpOnly: true, /** (boolean) httpOnly or not (default true) */
+    signed: true, /** (boolean) signed or not (default true) */
+    rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+    renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
+
+
 // 注入应用
 io.attach(app)
 
@@ -22,3 +38,6 @@ app.use(koaSend)
 app.use(koaStatic)
 app.use(bodyParser)
 app.use(Busboy)
+app.use(koaSession(SESSION_CONFIG))
+
+app.listen(3000)
